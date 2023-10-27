4cef72386e45dee14948e2a930339fd0e57071fc
davit-hayrapetyan <60064132+davit-hayrapetyan@users.noreply.github.com>
2023-10-27 20:34:44 +0400

    Fixed drone travers issue.

--- droneside/droneprocessor.py
+++ droneside/droneprocessor.py
@@ -34,13 +34,13 @@ class DronDataProcessor:
             with open(file_path, 'r') as file:
                 self.droneType = int(file.readline().strip())
                 self.droneId = int(file.readline().strip())
-                dronesCount = int(file.readline().strip())
-                for num in range(0, dronesCount):
+                self.dronesCount = int(file.readline().strip())
+                for num in range(0, self.dronesCount):
                     aDroneID = int(file.readline().strip())
                     aStartNodeID = int(file.readline().strip())
                     self.drones.append(Drone(aDroneID, aStartNodeID))
-                nodesCount = int(file.readline().strip())
-                for num in range(0, nodesCount):
+                self.nodesCount = int(file.readline().strip())
+                for num in range(0, self.nodesCount):
                     tmpNode = Node()
                     tmpNode.ID = int(file.readline().strip())
                     tmpNode.X = float(file.readline().strip())
@@ -58,25 +58,25 @@ class DronDataProcessor:
     def startProcessing(self, dronIp, isSimMode):
         vecDronesCurrPos = []
         vecDronesStartDir = []
-        vecCompleted = []
-        vecDroneLanded = []
+        vecLandedInNode = [] #about node
+        vecDroneLanded = [] #about drone
 
         for drone in self.drones:
             vecDronesCurrPos.append(drone.startNodeID)
             vecDronesStartDir.append(self.nodes[drone.startNodeID].CurrInd)
-            vecCompleted.append(False)
+            vecLandedInNode.append(False)
             vecDroneLanded.append(False)
 
         def checkCompleted():
             res = True
-            for droneRes in vecCompleted:
+            for droneRes in vecLandedInNode:
                 res = res and droneRes
             return res
 
         def isDroneAlowedToFinish(currDroneNum):
-            for i in range(len(self.drones)):
-                if vecDronesCurrPos[currDroneNum] == self.drones[i].startNodeID and self.nodes[vecDronesCurrPos[currDroneNum]].CurrInd == vecDronesStartDir[i] and (vecCompleted[i] == False):
-                    vecCompleted[i] = True
+            for i in range(0, self.dronesCount):
+                if vecDronesCurrPos[currDroneNum] == self.drones[i].startNodeID and self.nodes[vecDronesCurrPos[currDroneNum]].CurrInd == vecDronesStartDir[i] and (vecLandedInNode[i] == False):
+                    vecLandedInNode[i] = True
                     return True
             return False
         
@@ -84,7 +84,7 @@ class DronDataProcessor:
         self.droneControl.goto(self.nodes[self.drones[self.droneId].startNodeID].X, self.nodes[self.drones[self.droneId].startNodeID].Y)
         print('Go to home')
         while not checkCompleted():
-            for droneNum in range(len(vecDronesCurrPos)):
+            for droneNum in range(0, self.dronesCount):
                 if vecDroneLanded[droneNum] == False:
                     tmpInd = self.nodes[vecDronesCurrPos[droneNum]].CurrInd
                     tmpInd += 1
@@ -93,8 +93,8 @@ class DronDataProcessor:
                     print(tmpInd)
                     self.nodes[vecDronesCurrPos[droneNum]].CurrInd = tmpInd
                     vecDronesCurrPos[droneNum] = self.nodes[vecDronesCurrPos[droneNum]].NeighLst[tmpInd]
-                    print('Node id is', vecDronesCurrPos[droneNum])
                     if self.drones[droneNum].ID == self.droneId:
+                        print('Node id is', vecDronesCurrPos[droneNum])
                         # Agit generate command for send drone to new pos
                         # to get coords see
                         print('Drone next coords is', self.nodes[vecDronesCurrPos[droneNum]].X, self.nodes[vecDronesCurrPos[droneNum]].Y)
@@ -131,6 +131,7 @@ if __name__ == '__main__':
 
     file_path = os.path.abspath(args.data)  # Replace with the actual file path
 
+    # file_path = r'C:\Users\hayra\Desktop\IIAP_WORK\new\DCIS\droneside\examples\multidrone_0.data'
     processor = DronDataProcessor(file_path)
     processor.readFileContent()
 
